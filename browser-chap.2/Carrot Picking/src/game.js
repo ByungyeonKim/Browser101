f6d2a0ebbde45e9e30ee0e0d0673248ffb99e5ca
ByungyeonKim <viva_kby@naver.com>
2020-08-14 14:14:23 +0900

    JS: Builder Pattern

--- browser-chap.2/Carrot Picking/src/game.js
+++ browser-chap.2/Carrot Picking/src/game.js
@@ -2,7 +2,33 @@
 import Field from './field.js';
 import * as sound from './sound.js';
 
-export default class Game {
+// Builder Pattern - 가독성이 좋게
+export default class GameBuilder {
+  gameDuration(duration) {
+    this.gameDuration = duration;
+    return this;
+  }
+
+  carrotCount(num) {
+    this.carrotCount = num;
+    return this;
+  }
+
+  bugCount(num) {
+    this.bugCount = num;
+    return this;
+  }
+
+  build() {
+    return new Game(
+      this.gameDuration, //
+      this.carrotCount,
+      this.bugCount
+    );
+  }
+}
+
+class Game {
   constructor(gameDuration, carrotCount, bugCount) {
     this.gameDuration = gameDuration;
     this.carrotCount = carrotCount;
@@ -45,7 +71,7 @@ export default class Game {
     this.hideGameBtn();
     sound.playAlert();
     sound.stopBackground();
-    this.onGameStop && this.onGameStop();
+    this.onGameStop && this.onGameStop('cancel');
   }
 
   finish(win) {
--- browser-chap.2/Carrot Picking/src/main.js
+++ browser-chap.2/Carrot Picking/src/main.js
@@ -1,9 +1,15 @@
 'use strict';
 import PopUp from './popup.js';
-import Game from './game.js';
+import GameBuilder from './game.js';
 
 const gameFinishBanner = new PopUp();
-const game = new Game(20, 20, 20);
+const game = new GameBuilder()
+  .gameDuration(5)
+  .carrotCount(3)
+  .bugCount(3)
+  .build();
+// 생성자에 인자가 3개 이상 넘어가는 경우에는 이런식의 코딩은 실수할 가능성도 있고 좋지 않다.
+// const game = new Game(20, 20, 20);
 game.setGameStopListener((reason) => {
   console.log(reason);
   let message;
